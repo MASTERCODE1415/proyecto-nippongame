a218ce4f25a8812348df5c15e3efdbb0de976388
MASTERCODE1415 <l00772655@gmail.com>
2026-09-04 21:02:01 -0300

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    # . . . #
+    . # . # .
+    . . # . .
+    `)
+music.play(music.stringPlayable("B G E B G E A C5 ", 120), music.PlaybackMode.UntilDone)
+basic.clearScreen()
+basic.showString("¡Hola!")
+basic.showString("PROYECTO HIPERTRIX")
+music.play(music.createSoundExpression(WaveShape.Sine, 5000, 1, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
